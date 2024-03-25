fe928e78333967ed2754552e806cfa6a29f6c76a
maciker <ikermacaya@gmail.com>
2024-03-25 23:21:44 +0100

    Red: player Two Wins when reach 4 points with an advantage of 2

--- test/getTennisScore.spec.js
+++ test/getTennisScore.spec.js
@@ -4,4 +4,7 @@ describe('Tennis score test suite', () => {
     it('should return player one Wins when reach 4 points with an advantage of 2', () => {
         expect(getTennisScore(4, 0)).toEqual('Player One Wins')
     })
+    it('should return player Two Wins when reach 4 points with an advantage of 2', () => {
+        expect(getTennisScore(2, 4)).toEqual('Player Two Wins')
+    })
 })
