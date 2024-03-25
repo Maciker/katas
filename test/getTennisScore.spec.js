b2d1fdc62bdac1fa42b2d8647f337fc7b8a93218
maciker <ikermacaya@gmail.com>
2024-03-25 23:29:07 +0100

    Red: return Love-All if the players are tied at 0

--- test/getTennisScore.spec.js
+++ test/getTennisScore.spec.js
@@ -8,4 +8,7 @@ describe('Tennis score test suite', () => {
     it('should return player Two Wins when reach 4 points with an advantage of 2', () => {
         expect(getTennisScore(2, 4)).toEqual('Player Two Wins')
     })
+    it('should return "Love-All" if the players are tied at 0', () => {
+        expect(getTennisScore(0, 0)).toEqual('Love-All')
+    })
 })
