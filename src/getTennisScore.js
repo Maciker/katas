171dbb2cf91094470ce6985551d9a4d4a4c7d866
maciker <ikermacaya@gmail.com>
2024-03-25 23:20:27 +0100

    Green: player one Wins when reach 4 points with an advantage of 2

--- src/getTennisScore.js
+++ src/getTennisScore.js
@@ -1,4 +1,7 @@
 const getTennisScore = (playerOneScore, playerTwoScore) => {
+    if (playerOneScore >= 4 && playerTwoScore <= 2) {
+        return "Player One Wins"
+    }
     return 0
 }
 export default getTennisScore
