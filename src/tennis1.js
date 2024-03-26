d66e07f8d0716ff5f2cf6e3816e155f8b3615458
maciker <ikermacaya@gmail.com>
2024-03-26 21:57:11 +0100

    Refactor: encapsulating when boths scores are above four

--- src/tennis1.js
+++ src/tennis1.js
@@ -16,16 +16,17 @@ function getScore(m_score1, m_score2) {
         return `${scoreSyntax[m_score1]}-All`
     }
     const scoreRemainder = m_score1 - m_score2;
-    if ((m_score1 >= 4 || m_score2 >= 4) && scoreRemainder === 1) {
+    const bothScoresAboveFour = m_score1 >= 4 || m_score2 >= 4;
+    if (bothScoresAboveFour && scoreRemainder === 1) {
         return "Advantage player1";
     }
-    if ((m_score1 >= 4 || m_score2 >= 4) && scoreRemainder === -1) {
+    if (bothScoresAboveFour && scoreRemainder === -1) {
         return "Advantage player2";
     }
-    if ((m_score1 >= 4 || m_score2 >= 4) && scoreRemainder >= 2) {
+    if (bothScoresAboveFour && scoreRemainder >= 2) {
         return "Win for player1";
     }
-    if ((m_score1 >= 4 || m_score2 >= 4) && scoreRemainder <= -2) {
+    if (bothScoresAboveFour && scoreRemainder <= -2) {
         return "Win for player2";
     }
 
