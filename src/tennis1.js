cadee4a4c0d76671d345bf049a16f703712687fc
maciker <ikermacaya@gmail.com>
2024-03-26 21:59:04 +0100

    Refactor: deleting loops and return directly the players scores

--- src/tennis1.js
+++ src/tennis1.js
@@ -30,28 +30,7 @@ function getScore(m_score1, m_score2) {
         return "Win for player2";
     }
 
-    for (let i = 1; i < 3; i++) {
-        if (i === 1) {tempScore = m_score1;}
-        else {
-            score += "-";
-            tempScore = m_score2;
-        }
-        switch (tempScore) {
-            case 0:
-                score += "Love";
-                break;
-            case 1:
-                score += "Fifteen";
-                break;
-            case 2:
-                score += "Thirty";
-                break;
-            case 3:
-                score += "Forty";
-                break;
-        }
-    }
-    return score;
+    return `${scoreSyntax[m_score1]}-${scoreSyntax[m_score2]}`;
 }
 
 module.exports = getScore;
