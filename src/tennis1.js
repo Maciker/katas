c8b5094686c2bf85bc425d9f87ec3f02de4fbf0a
maciker <ikermacaya@gmail.com>
2024-03-26 21:41:52 +0100

    Refactor: Return score directly when a player win or one of them have advantage. Un-nesting conditional statments

--- src/tennis1.js
+++ src/tennis1.js
@@ -14,12 +14,21 @@ function getScore(m_score1, m_score2) {
             default:
                 return "Deuce";
         }
-    } else if (m_score1 >= 4 || m_score2 >= 4) {
+    }
+    if (m_score1 >= 4 || m_score2 >= 4) {
         let minusResult = m_score1 - m_score2;
-        if (minusResult === 1) {score = "Advantage player1";}
-        else if (minusResult === -1) {score = "Advantage player2";}
-        else if (minusResult >= 2) {score = "Win for player1";}
-        else {score = "Win for player2";}
+        if (minusResult === 1) {
+            return "Advantage player1";
+        }
+        if (minusResult === -1) {
+            return "Advantage player2";
+        }
+        if (minusResult >= 2) {
+            return "Win for player1";
+        }
+        if (minusResult <= -2) {
+            score = "Win for player2";
+        }
     } else {
         for (let i = 1; i < 3; i++) {
             if (i === 1) {tempScore = m_score1;}
