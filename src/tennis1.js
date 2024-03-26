54a89d836af156be707e33094457fa7fc9d3cb3d
maciker <ikermacaya@gmail.com>
2024-03-26 21:55:32 +0100

    Refactor: conditional statments and remainder naming

--- src/tennis1.js
+++ src/tennis1.js
@@ -15,41 +15,39 @@ function getScore(m_score1, m_score2) {
         }
         return `${scoreSyntax[m_score1]}-All`
     }
-    const minusResult = m_score1 - m_score2;
-    if (m_score1 >= 4 || m_score2 >= 4) {
-        if (minusResult === 1) {
-            return "Advantage player1";
+    const scoreRemainder = m_score1 - m_score2;
+    if ((m_score1 >= 4 || m_score2 >= 4) && scoreRemainder === 1) {
+        return "Advantage player1";
+    }
+    if ((m_score1 >= 4 || m_score2 >= 4) && scoreRemainder === -1) {
+        return "Advantage player2";
+    }
+    if ((m_score1 >= 4 || m_score2 >= 4) && scoreRemainder >= 2) {
+        return "Win for player1";
+    }
+    if ((m_score1 >= 4 || m_score2 >= 4) && scoreRemainder <= -2) {
+        return "Win for player2";
+    }
+
+    for (let i = 1; i < 3; i++) {
+        if (i === 1) {tempScore = m_score1;}
+        else {
+            score += "-";
+            tempScore = m_score2;
         }
-        if (minusResult === -1) {
-            return "Advantage player2";
-        }
-        if (minusResult >= 2) {
-            return "Win for player1";
-        }
-        if (minusResult <= -2) {
-            score = "Win for player2";
-        }
-    } else {
-        for (let i = 1; i < 3; i++) {
-            if (i === 1) {tempScore = m_score1;}
-            else {
-                score += "-";
-                tempScore = m_score2;
-            }
-            switch (tempScore) {
-                case 0:
-                    score += "Love";
-                    break;
-                case 1:
-                    score += "Fifteen";
-                    break;
-                case 2:
-                    score += "Thirty";
-                    break;
-                case 3:
-                    score += "Forty";
-                    break;
-            }
+        switch (tempScore) {
+            case 0:
+                score += "Love";
+                break;
+            case 1:
+                score += "Fifteen";
+                break;
+            case 2:
+                score += "Thirty";
+                break;
+            case 3:
+                score += "Forty";
+                break;
         }
     }
     return score;
