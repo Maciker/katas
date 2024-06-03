70a7b8e20c7a04b0c5fa8af9728a8d8e35c3e5f9
Iker Macaya Faber <iker@macaya.dev>
2024-06-03 23:28:34 +0200

    Added 'Sulfuras, Hand of Ragnaros' test

--- test/gilded-rose/gilded_rose.test.js
+++ test/gilded-rose/gilded_rose.test.js
@@ -1,9 +1,10 @@
 const {Shop, Item} = require("../../src/gilded-rose/gilded_rose");
 
 describe("Gilded Rose", function() {
-    it("should foo", function() {
-        const gildedRose = new Shop([new Item("foo", 0, 0)]);
+    it("should not change the item: Sulfuras, Hand of Ragnaros ", function() {
+        const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 1, 80)]);
         const items = gildedRose.updateQuality();
-        expect(items[0].name).toBe("fixme");
+        expect(items[0].name).toBe("Sulfuras, Hand of Ragnaros");
+        expect(items[0].quality).toBe(80);
     });
 });
