69d59ec73ba131a0b456a3ac0f944542f588264b
Iker Macaya Faber <iker@macaya.dev>
2024-06-06 22:12:55 +0200

    Add 3 items to test

--- test/gilded-rose/gilded_rose.test.js
+++ test/gilded-rose/gilded_rose.test.js
@@ -1,10 +1,13 @@
 const {Shop, Item} = require("../../src/gilded-rose/gilded_rose");
 
-describe("Gilded Rose", function() {
+const sulfurasHandofRagnaros = new Shop([new Item("Sulfuras, Hand of Ragnaros", 1, 80)]);
+const agedBrie = new Shop([new Item("Aged Brie", 10, 20)]);
+const backstageConcert = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 5, 25)]);
+
+describe("Gilded Rose - Items rules", function() {
     it("should not change the item: Sulfuras, Hand of Ragnaros ", function() {
-        const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 1, 80)]);
-        const items = gildedRose.updateQuality();
+        const items = sulfurasHandofRagnaros.updateQuality();
         expect(items[0].name).toBe("Sulfuras, Hand of Ragnaros");
         expect(items[0].quality).toBe(80);
     });
-});Item
+});
