d8895b54a88dbfae9e2bd7d8f993c94021cadd72
maciker <ikermacaya@gmail.com>
2025-01-27 10:09:46 +0100

    Bowling kata: implement strike calculation

--- src/bowling-game/game.js
+++ src/bowling-game/game.js
@@ -13,19 +13,38 @@ class Game {
         let frameIndex = 0;
         
         for (let frame = 0; frame < 10; frame++) {
-            if (this.isSpare(frameIndex)) {
-                score += 10 + this.rolls[frameIndex + 2];
+            if (this.isStrike(frameIndex)) {
+                score += 10 + this.strikeBonus(frameIndex);
+                frameIndex++;
+            } else if (this.isSpare(frameIndex)) {
+                score += 10 + this.spareBonus(frameIndex);
                 frameIndex += 2;
             } else {
-                score += this.rolls[frameIndex] + this.rolls[frameIndex + 1];
+                score += this.sumOfBallsInFrame(frameIndex);
                 frameIndex += 2;
             }
         }
         return score;
     }
 
+    isStrike(frameIndex) {
+        return this.rolls[frameIndex] === 10;
+    }
+
     isSpare(frameIndex) {
         return this.rolls[frameIndex] + this.rolls[frameIndex + 1] === 10;
+    }
+
+    strikeBonus(frameIndex) {
+        return this.rolls[frameIndex + 1] + this.rolls[frameIndex + 2];
+    }
+
+    spareBonus(frameIndex) {
+        return this.rolls[frameIndex + 2];
+    }
+
+    sumOfBallsInFrame(frameIndex) {
+        return this.rolls[frameIndex] + this.rolls[frameIndex + 1];
     }
 }
 
