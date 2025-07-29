2684a2f8f7640bb68522f3913ffffaa03da1e0a8
maciker <ikermacaya@gmail.com>
2025-07-29 11:49:45 +0200

    Red: should score 20 in a full 1s game

--- typescript/src/tests/01_bowling.test.ts
+++ typescript/src/tests/01_bowling.test.ts
@@ -12,4 +12,11 @@ describe("Bowling Game", () => {
         }
         expect(bowlingGame.totalScore()).toBe(0);
     })
+
+    it('should score 20 in a full 1s game', () => {
+        for (let i = 0; i < 20; i++) {
+            bowlingGame.roll(1);
+        }
+        expect(bowlingGame.totalScore()).toBe(20);
+    })
 });
