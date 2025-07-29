cfe2060c253de5d74b61d29ac2389473339c4a6f
maciker <ikermacaya@gmail.com>
2025-07-29 11:42:19 +0200

    Instance of BowlingGame added before each test

--- typescript/src/tests/01_bowling.test.ts
+++ typescript/src/tests/01_bowling.test.ts
@@ -1,7 +1,13 @@
 import  BowlingGame from '../core/01_bowling';
+import Game from "../core/00_bowling";
 describe("Bowling Game", () => {
+    let game: BowlingGame;
+
+    beforeEach(() => {
+        game = new BowlingGame();
+    });
+
     it('should return 0 for no rolls', () => {
-        const game = new BowlingGame();
         expect(game.totalScore()).toBe(0);
     })
 });
